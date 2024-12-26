8b9a5c4fff51738f9aac521acb34909a0e4c69b3
mazenSakr <mazen_sakr@outlook.com>
2024-12-26 04:00:59 +0200

    Added feature to view tasks in the to-do list.

--- to-do-list.py
+++ to-do-list.py
@@ -1,4 +1,13 @@
+
 tasks = [] 
 def add_task(task): 
-tasks.append(task) 
-print(f"Task added: {task}")
+    tasks.append(task) 
+    print(f"Task added: {task}")
+
+def view_tasks(): 
+    if not tasks: 
+        print("Your to-do list is empty.") 
+    else: 
+        print("Your tasks:") 
+    for i, task in enumerate(tasks, start=1): 
+        print(f"{i}. {task}") 
